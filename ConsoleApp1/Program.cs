d0e6f129e2c10a1ca57f40e3883f33e7a7e11bb6
DevAgui <dagmartin98@gmail.com>
2022-10-04 14:17:55 +0200

    First change

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
 
-            System.Console.WriteLine("Hola Mundo");
+            System.Console.WriteLine("Hola Mundo!!!");
 
         }
     }
